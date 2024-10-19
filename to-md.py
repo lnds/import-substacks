6850372ded65bb6d4bc76dee53b76004037710bd
Eduardo Díaz <ediaz@dcc8090.cl>
2024-10-19 20:17:12 -0300

    feat(to-md.py): resolve internal urls

--- to-md.py
+++ to-md.py
@@ -8,11 +8,13 @@ import requests
 import uuid
 from urllib.parse import urlparse
 
-
 csv_filename = sys.argv[1] if len(sys.argv) > 1 else 'posts.csv'
 in_dir = sys.argv[2] if len(sys.argv) > 2 else 'posts'
 out_dir = sys.argv[3] if len(sys.argv) > 3 else 'md_posts'
 from_date = sys.argv[4] if len(sys.argv) > 4 else '2022-12-18'
+base_prefix = sys.argv[5] if len(sys.argv) > 5 else 'https://newsletter.lnds.net/p/'
+
+md_urls = dict()
 
 if not os.path.exists(out_dir):
     os.mkdir(out_dir)
@@ -37,6 +39,15 @@ class ImageBlockConverter(MarkdownConverter):
     def convert_hn(self, n, el, text, convert_as_inline):
         return f'\n\n{super().convert_hn(n, el, text, convert_as_inline)}'
 
+    def convert_a(self, el, text, convert_as_inline):
+        href = el['href']
+        if href.startswith(base_prefix):
+            path = href.removeprefix(base_prefix).split('?')
+            key = path[0] if len(path) > 0 else path
+            if key in md_urls:
+                el['href'] = md_urls[key]
+        return super().convert_a(el, text, convert_as_inline)
+
 # Create shorthand method for conversion
 def md(html, **options):
     return ImageBlockConverter(**options).convert(html)
@@ -46,7 +57,8 @@ def process_post(post_id, post_date, post_title, post_subtitle):
     with open(p, 'r') as f:
         text = f.read()
         markdown = md(text)
-        o = Path(out_dir) / Path(f'{post_id}.md')
+        in_url = f'{post_id}.md'
+        o = Path(out_dir) / Path(in_url)
         with open(o, 'w') as of:
             of.write('---\n')
             of.write(f'title: {post_title}\n')
@@ -60,7 +72,16 @@ def process_post(post_id, post_date, post_title, post_subtitle):
                 of.write(f'**{post_subtitle}**\n\n')
             of.write(markdown)
 
+# pre process internal urls
+with open(csv_filename) as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        post_id = row['post_id']
+        _, k = post_id.split('.') 
+        in_url = f'{post_id}.md'
+        md_urls[k] = in_url
 
+# process files
 with open(csv_filename) as f:
     reader = csv.DictReader(f)
     for row in reader:
